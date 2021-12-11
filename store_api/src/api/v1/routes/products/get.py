7186b9988420e07a6275c9bb594c0539e82282a7
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-11 20:57:11 -0300

    feat: Create route to get a product by its id

---
+++ store_api/src/api/v1/routes/products/get.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException
+from models.v1.products import ProductSchemaID
+from api.v1.crud.products.get import get
+
+
+router = APIRouter()
+
+@router.get('/products/{product_id}', response_model=ProductSchemaID, status_code = 200)
+async def get_product_by_id(product_id: int):
+    ''' Get an especific existent product by its ID'''
+    if product_id < 1:
+        raise HTTPException(status_code=400, detail="Product ID must be greater than 0")
+
+    product = await get(product_id)
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return product
