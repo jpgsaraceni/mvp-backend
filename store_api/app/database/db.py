e50106ee6ef75c25d077b56bc57d4c9c8736b6f9
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-19 19:44:15 -0300

    chore: Create new tables on the database and update products table

--- store_api/app/database/db.py
+++ store_api/app/database/db.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     Column,
     DateTime,
     Float,
+    ForeignKey,
     Integer,
     String,
     Table,
@@ -32,6 +33,20 @@ if not engine.dialect.has_schema(engine, DB_SCHEMA):
 
 metadata = MetaData(schema=DB_SCHEMA) # used for creating the database schema
 
+#define table categories
+categories = Table (
+    'categories',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(30), nullable=False),
+    Column('description', String(140), nullable=True),
+    Column('created_at', DateTime(timezone=True), default=func.now(), nullable=False),
+    Column('inactivated_at', DateTime(timezone=True), nullable=True),
+    Column('updated_at', DateTime(timezone=True), nullable=True),
+    Column('updated_at', DateTime(timezone=True), nullable=True),
+    Column('deleted_at', DateTime(timezone=True), nullable=True)
+)
+
 # define table products
 products = Table (
     'products',
@@ -39,14 +54,31 @@ products = Table (
     Column('id', Integer, primary_key=True),
     Column('name', String(50), nullable=False),
     Column('description', String(140), nullable=False),
+    Column('category_id', ForeignKey('categories.id'), nullable=False),
     Column('price', Float, nullable=False),
     Column('image', Text, nullable=True),
     Column('created_at', DateTime(timezone=True), default=func.now(), nullable=False),
+    Column('inactivated_at', DateTime(timezone=True)),
     Column('updated_at', DateTime(timezone=True)),
     Column('deleted_at', DateTime(timezone=True)),
 )
 
-# create table products on the database
+# define table sales
+sales = Table(
+    'sales',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('client_id', Integer, nullable=False),
+    Column('product_id', ForeignKey('products.id'), nullable=False),
+    Column('amount', Integer, nullable=False, default=1),
+    Column('payment_method_id', Integer, nullable=False),
+    Column('price', Float, nullable=False),
+    Column('valid_until', DateTime(timezone=True), nullable=True),
+    Column('created_at', DateTime(timezone=True), default=func.now(), nullable=False),
+    Column('deleted_at', DateTime(timezone=True)),
+)
+
+# create tables on the database
 metadata.create_all(engine)
 
 # database query builder
