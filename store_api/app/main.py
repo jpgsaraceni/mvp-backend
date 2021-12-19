7c263650e7df3adf17e0200a8f8be736d9640615
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-19 20:10:28 -0300

    feat: Expose v2 routes

--- store_api/app/main.py
+++ store_api/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from app.api.v1.routes.products import main as products_routes
+from app.api.v1.routes.products import main as v1_routes
+from app.api.v2.routes import main as v2_routes
 
 app = FastAPI()
 
-app.include_router(products_routes.router)
+app.include_router(v1_routes.router)
+
+app.include_router(v2_routes.router)
