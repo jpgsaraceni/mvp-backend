e7d6c08b2de52205bea99a3595446e228880f4e2
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-19 20:09:31 -0300

    chore: Import v2 routes

---
+++ store_api/app/api/v2/routes/main.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from app.api.v2.routes.products import main as products_routes
+from app.api.v2.routes.sales import main as sales_routes
+from app.api.v2.routes.categories import main as categories_routes
+
+router = APIRouter(
+    prefix='/v2',
+    tags=["v2"]
+)
+
+router.include_router(products_routes.router)
+router.include_router(sales_routes.router)
+router.include_router(categories_routes.router)
