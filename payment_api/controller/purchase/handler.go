8918933b61d62353c5973de52194e42803cf0f2c
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-21 16:23:37 -0300

    refactor: Move purchase route from main.go

---
+++ payment_api/controller/purchase/handler.go
@@ -0,0 +1,23 @@
+package purchase
+
+import (
+	"encoding/json" // "encoding/json" // package to encode and decode the json into struct and vice versa
+
+	"net/http" // used to access the request and response object of the api
+)
+
+// authorize receives a request and, if the method is POST, writes a json with status: accepted.
+func Authorize(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if req.Method == "POST" {
+		response := map[string]string{
+			"payment": "confirmed",
+		}
+
+		json.NewEncoder(w).Encode(response)
+
+		return
+	}
+	http.Error(w, "Invalid request method.", http.StatusMethodNotAllowed)
+}
