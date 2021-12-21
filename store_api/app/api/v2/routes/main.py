c6945d7b5d583d033618dbf1f2205dcf6c92d026
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-12-21 16:40:39 -0300

    fix: Hide sales routes

--- store_api/app/api/v2/routes/main.py
+++ store_api/app/api/v2/routes/main.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.api.v2.routes.products import main as products_routes
-from app.api.v2.routes.sales import main as sales_routes
 from app.api.v2.routes.categories import main as categories_routes
 
 router = APIRouter(
@@ -9,5 +8,4 @@ router = APIRouter(
 )
 
 router.include_router(products_routes.router)
-router.include_router(sales_routes.router)
 router.include_router(categories_routes.router)
